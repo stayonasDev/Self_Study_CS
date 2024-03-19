4e00977ccd1829a48f56c63dc1fe5344d6391412
yoon <stay_on_track@naver.com>
2024-03-19 16:24:40 +0900

    So Hard Add List interface, ArrayList Class

--- Data_Structure/Data_Structure/src/list/MakeList.java
+++ Data_Structure/Data_Structure/src/list/MakeList.java
@@ -1,7 +1,17 @@
 package list;
 
-//TODO
-public interface MakeList {
+import java.util.Collection;
+
+public interface MakeList <E>{
     public abstract void add();
-    public abstract void remove();
+    public abstract void addAll(int index, Collection<? extends E> c);
+
+    public abstract  E get(int index);
+    public abstract E remove(int index);
+    public abstract  void clear();
+    public abstract boolean contains();
+    public abstract int indexOf(Object o);
+    public abstract boolean isEmpty();
+    public abstract  int size();
+
 }
